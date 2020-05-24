d66feeb1b342138c95538fe5673cb0e4248de3d2
Oliver <oliver.buckler@gmail.com>
2020-05-24 20:09:49 +0800

    feat/ get all + error checking

--- Main.sh
+++ Main.sh
@@ -6,8 +6,35 @@
 
 urlSpecificImage="https://secure.ecu.edu.au/service-centres/MACSC/gallery/152/"
 urlImages="https://www.ecu.edu.au/service-centres/MACSC/gallery/gallery.php?folder=152"
-images=$(wget -q $urlImages -O /dev/stdout | grep -Po "(DSC\d{5})" | sort --unique)
+images=($(wget -q $urlImages -O /dev/stdout | grep -Po "(DSC\d{5})" | sort --unique))
+localDirectory="./fetched"
 
+# Initial setup required for the application to
+# run. Builds the storage directory for images.
+function setup {
+    if ! test -d "${localDirectory}";
+    then
+        mkdir -p ${localDirectory}
+    fi
+}
+
+# Checks if the id provided exists 
+# as a image on the disk.
+function doesImageExistOnDisk {
+    localPath="./$1.jpg"
+
+    if test -f "${localDirectory}/${localPath}";
+    then
+        return 0
+    fi
+
+    return 1
+}
+
+# Given an image id, it will attempt to
+# pull the images properties using curl
+# and finally return the size of the image
+# in kilobytes.
 function getImageSize {
     url="${urlSpecificImage}/${id}.jpg"
     result=$(curl -sI $url | grep -i "Content-Length" | grep -Po "(\d)+")
@@ -15,23 +42,69 @@ function getImageSize {
     echo "${formatted}kb"
 }
 
+# Given an id it will attempt to download
+# it using wget, after wget finishes it then
+# checks on disk to see if the image has 
+# succesfully downloaded.
+function downloadImage {
+    url="${urlSpecificImage}/$1.jpg"
+    outputFile="$1.jpg"
+    imageSize=$(getImageSize $1)
+
+    echo "Downloading \"$1\" with the file name ${outputFile}, with a file size of ${imageSize}"
+    result=$(wget -q ${url} -O ${localDirectory}/${outputFile})
+
+    if doesImageExistOnDisk $1;
+    then
+        echo "[OK] Download completed!"
+    else
+        echo "[ERR] Download failed to complete."
+    fi
+}
+
+# Checks if the data from the webserver 
+# contains a specific image id.
+function doesImageExist {
+    if [[ " ${images[@]} " =~ "${id}" ]];
+    then
+        return 0
+    else 
+        echo "[ERR] Couldn't find the specified '${id}' in the list of available images."
+        return 1
+    fi
+
+}
+
+# Attempts to download a specific image 
+# via an id provided by the user
 function GetSpecific {
     echo "GetSpecific()"
 
     read -p "Enter image url: " id
 
-    if [[ " ${images[@]} " =~ "${id}" ]];
+    if doesImageExist ${id}
     then
-        echo "[OK] ${id}"
-        imageSize=$(getImageSize ${id})
-        echo "Downloading ${id} with the file name ${id}.jpg, with a file size of ${imageSize}..."
-    else 
-        echo "[ERR] Couldn't find the specified '${id}'."
+        downloadImage ${id}
     fi
 }
 
+# Attempts to download all images
 function GetAll {
-    echo "GetAll()"
+    count="${#images[@]}"
+    # echo "[${images[$id]}/${count}]"
+    echo "Downloading ${count} image(s)."
+    for id in "${!images[@]}"
+    do
+        item="${images[$id]}"
+        echo "[${id}/$count] - ${item}"
+
+        if ! doesImageExistOnDisk ${item};
+        then
+            downloadImage ${item}
+        else
+            echo "Image already exists, skipping."
+        fi
+    done
 }
 
 function GetInRange {
@@ -42,6 +115,8 @@ function GetRandomAmount {
     echo "GetRandomAmount()"
 }
 
+
+setup
 clear
 cat <<EOF
 ==============================
@@ -64,5 +139,3 @@ case "$REPLY" in
     "4") GetRandomAmount ;;
     *  ) ;;
 esac
-
-# Main
